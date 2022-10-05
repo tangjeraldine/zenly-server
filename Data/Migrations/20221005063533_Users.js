ab7e75411d854240793d570fe229491a84edf277
tangjeraldine <typjera.1127@gmail.com>
2022-10-06 01:54:55 +0800

    Built schemas using knex and migrated successfully.

--- Data/Migrations/20221005063533_Users.js
+++ Data/Migrations/20221005063533_Users.js
@@ -2,14 +2,148 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
-};
 
+// const createTablesSafely = (knex) => (tables) => {
+//   const createTables = tables.map(({ name, schema }) => {
+//     return knex.schema.createTable(name, schema);
+//   });
+
+//   return Promise.all(createTables).catch((e) => {
+//     const dropTables = tables.map(({ name }) => {
+//       return knex.schema.dropTableIfExists(name);
+//     });
+
+//     return Promise.all(dropTables).then(() => Promise.reject(e));
+//   });
+// };
+
+exports.up = (knex, Promise) => {
+  return knex.schema
+    .createTable("Users", function (tbl) {
+      tbl.primary(["id"]);
+      tbl.increments("id").checkPositive().notNullable();
+      tbl.string("full_name", 40).notNullable();
+      tbl.unique(["email"]);
+      tbl
+        .string("email", 40)
+        .checkRegex("^([a-zA-Z0-9_-.]+)@([a-zA-Z0-9_-.]+).([a-zA-Z]{2,5})$")
+        .notNullable();
+      tbl
+        .string("password")
+        .checkRegex(
+          "^(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[@$!%*?&])[A-Za-zd@$!%*?&]{8,18}$"
+        )
+        .notNullable();
+      tbl
+        .string("phone_no")
+        .checkRegex("[0-9]{8}")
+        .checkLength("<=", 15)
+        .notNullable();
+      tbl.integer("security_lvl").defaultTo("1");
+      tbl.timestamp("created_at").defaultTo(knex.fn.now());
+    })
+    .createTable("Goods", function (tbl) {
+      tbl.primary(["id"]);
+      tbl.increments("id").checkPositive().notNullable();
+      tbl.unique(["title"]);
+      tbl.string("title", 60).notNullable();
+      tbl.string("image_url", 200).notNullable();
+      tbl.text("description").notNullable();
+      tbl.enu("goods_type", ["Product", "Service"]).notNullable();
+      tbl.float("price", 5, 2).notNullable();
+      tbl.timestamp("created_at").defaultTo(knex.fn.now());
+    })
+    .createTable("Cart", function (tbl) {
+      tbl.primary(["id"]);
+      tbl.increments("id").checkPositive().notNullable();
+      tbl.integer("quantity").checkPositive().notNullable();
+      tbl
+        .integer("Users_id")
+        .unsigned()
+        .references("Users.id")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE")
+        .notNullable();
+      tbl
+        .integer("Goods_id")
+        .unsigned()
+        .references("Goods.id")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE")
+        .notNullable();
+      tbl.string("link_to", 50);
+      tbl.boolean("checked_out").defaultTo(false);
+    })
+    .createTable("Purchases", function (tbl) {
+      tbl.primary(["id"]);
+      tbl.increments("id").checkPositive().notNullable();
+      tbl
+        .integer("Users_id")
+        .unsigned()
+        .references("Users.id")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE")
+        .notNullable();
+      tbl
+        .integer("Goods_id")
+        .unsigned()
+        .references("Goods.id")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE")
+        .notNullable();
+      tbl.float("purchase_price", 5, 2).notNullable();
+      tbl.integer("quantity").checkPositive().notNullable();
+      tbl.timestamp("created_at").defaultTo(knex.fn.now());
+      tbl.string("transaction_no", 30).notNullable();
+      tbl
+        .enu("order_status", [
+          "Pending Confirmation",
+          "Session Booked",
+          "Order Completed",
+          "Order Cancelled",
+        ])
+        .defaultTo("Pending Confirmation");
+      tbl.float("grand_total", 6, 2).defaultTo(0);
+    })
+    .createTable("Appointments", function (tbl) {
+      tbl.primary(["id"]);
+      tbl.increments("id").checkPositive().notNullable();
+      tbl
+        .integer("Users_id")
+        .unsigned()
+        .references("Users.id")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE")
+        .notNullable();
+      tbl
+        .integer("Goods_id")
+        .unsigned()
+        .references("Goods.id")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE")
+        .notNullable();
+      tbl.string("date_time").notNullable();
+      tbl.text("visit_purpose", 300).notNullable();
+      tbl
+        .enu("appt_status", [
+          "Pending Confirmation",
+          "Session Confirmed",
+          "Session Completed",
+          "Session Cancelled",
+        ])
+        .defaultTo("Pending Confirmation");
+    });
+};
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = (knex, Promise) => {
+  return knex.schema
+
+    .dropTableIfExists("Cart")
+    .dropTableIfExists("Purchases")
+    .dropTableIfExists("Appointments")
+    .dropTableIfExists("Users")
+    .dropTableIfExists("Goods");
 };
--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,7 @@
 // Update with your config settings.
-const DB_URL = process.env.DB_URL;
+const DB_URL =
+  process.env.DB_URL ??
+  "postgres://pwddrxwa:d7fNZo35tih9nkXrVRdwPI-hF3jonNhN@tiny.db.elephantsql.com/pwddrxwa";
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
